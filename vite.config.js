40fdb5217263bd788817f4281beef3f2236a6b25
Magnus Fjeldstad <magnusfjeld1234@gmail.com>
2026-04-16 20:48:52 +0200

    pages fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const pagesBasePath = process.env.BASE_PATH || process.env.GITHUB_REPOSITORY?.split('/')[1];
+
 export default defineConfig({
-  base: './',
+  base: pagesBasePath ? `/${pagesBasePath}/` : '/',
   plugins: [react()],
 });
